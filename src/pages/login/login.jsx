78c9d4450412aa564c8e7386d755ce2317d951c9
Lorran Loshy <lorran.justo@hotmail.com>
2025-09-12 18:23:53 -0300

    First Commit

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './style.css'
+
+function Login() {
+  return (
+    <div className="main-container-box">
+      <div className="container-box">
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
