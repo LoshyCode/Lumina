2921fa2b5ee8060a048a48eb47187aa9ead378b7
Lorran Loshy <lorran.justo@hotmail.com>
2025-09-13 03:31:02 -0300

    Homepage commit 1, not finish

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/login/login.jsx'
+import Homepage from './pages/homepage/homepage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <Homepage />
   </StrictMode>,
 )
